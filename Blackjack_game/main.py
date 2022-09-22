371a1c6de0179f7effa1d20d4271c75337dff602
Everezze <83906211+Everezze@users.noreply.github.com>
2022-09-22 02:12:08 +0200

    checking winners and losers by comparing dealer and players card

--- Blackjack_game/main.py
+++ Blackjack_game/main.py
@@ -11,6 +11,7 @@ def main():
     PLAYERS_TURN = 1
     PLAYERS_NUMBER = [[],[]]
     PLAYERS_MONEY = {}
+    PLAYERS_BET = [0]
     PLAYER_OPTIONS = ['(H)it','(S)tand']
     PLAYER_ADDITIONAL_OPTIONS = ['(Do)uble','(Sp)lit','(Sur)render']
 
@@ -25,9 +26,10 @@ def main():
             player_hand.append(deck.pop(0))
 
     #print(PLAYERS)
-    PLAYER_BET =get_bet(PLAYERS_TURN,PLAYERS_MONEY)
 
     while PLAYERS_TURN:
+        CURRENT_BET =get_bet(PLAYERS_TURN,PLAYERS_MONEY)
+        PLAYERS_BET.append(CURRENT_BET)
 
         if len(PLAYERS_NUMBER[PLAYERS_TURN]) == 2:
             for option in PLAYER_ADDITIONAL_OPTIONS:
@@ -37,20 +39,38 @@ def main():
         else:
             PLAYER_OPTIONS = [x for x in PLAYER_OPTIONS if x not in PLAYER_ADDITIONAL_OPTIONS]
 
-        check_and_process_move(PLAYERS_NUMBER,PLAYERS_TURN,PLAYER_OPTIONS,PLAYERS_MONEY,PLAYER_BET,deck)
+        check_and_process_move(PLAYERS_NUMBER,PLAYERS_TURN,PLAYER_OPTIONS,PLAYERS_MONEY,CURRENT_BET,deck)
 
         card_sum = get_cards_value(PLAYERS_NUMBER[PLAYERS_TURN])
         if card_sum >21:
             print("Bust! You lost your bet,try another time")
-            PLAYERS_MONEY[PLAYERS_TURN] = PLAYERS_MONEY[PLAYERS_TURN] - PLAYER_BET
+            PLAYERS_MONEY[PLAYERS_TURN] -=  CURRENT_BET
 
         if PLAYERS_NUMBER[PLAYERS_TURN] == PLAYERS_NUMBER[-1]:
+            PLAYERS_TURN=0
             break
         else:
             PLAYERS_TURN +=1
-            PLAYER_BET =get_bet(PLAYERS_TURN,PLAYERS_MONEY)
 
     #now make the dealer draw cards until he's above 17
+    while get_cards_value(PLAYERS_NUMBER[PLAYERS_TURN]) < 17:
+        PLAYERS_NUMBER[PLAYERS_TURN].append(deck.pop(0))
+
+    if get_cards_value(PLAYERS_NUMBER[PLAYERS_TURN]) > 21:
+        for player in range(1,len(PLAYERS_NUMBER)):
+            if get_cards_value(PLAYERS_NUMBER[player]) <=21:
+                PLAYERS_MONEY[player] += PLAYERS_BET[player]
+    else:
+        for player in range(1,len(PLAYERS_NUMBER)):
+            if get_cards_value(PLAYERS_NUMBER[player]) <= 21 :
+                if get_cards_value(PLAYERS_NUMBER[player]) > get_cards_value(PLAYERS_NUMBER[PLAYERS_TURN]):
+                    # pay the player bet 
+                    ...
+                elif get_cards_value(PLAYERS_NUMBER[player]) < get_cards_value(PLAYERS_NUMBER[PLAYERS_TURN]):
+                    #get player money
+                    ...
+
+
 
 def make_deck(*forms):
 
@@ -70,10 +90,10 @@ def make_deck(*forms):
 
 def get_bet(player_turn, player_money):
     while True:
-        PLAYER_BET =input(f"Player {player_turn},Amount you're betting: ")
-        if PLAYER_BET.isnumeric() and int(PLAYER_BET) <= player_money[player_turn]:
-            return int(PLAYER_BET)
-        elif PLAYER_BET.isnumeric() and int(PLAYER_BET) > player_money[player_turn]:
+        CURRENT_BET =input(f"Player {player_turn},Amount you're betting: ")
+        if CURRENT_BET.isnumeric() and int(CURRENT_BET) <= player_money[player_turn]:
+            return int(CURRENT_BET)
+        elif CURRENT_BET.isnumeric() and int(CURRENT_BET) > player_money[player_turn]:
             print("You\'re betting money you don't have in your treasury")
         else:
             print("Please insert a valid amount")
